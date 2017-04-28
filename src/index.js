6a8e952af21c3fe4e419f34a827aab3d5e04963a
Brendon Blackwell <brendonblackwell@gmail.com>
2017-04-28 18:57:10 +0200

    pull from rob

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ function CubicBezier(P0, P1, P2, P3)
 {
     let scalar = function(t, p0, p1, p2, p3){
 
-        var res =   p0*(1-t)*(1-t)*(1-t) 
-					+ 3.0 * p1 * (1-t)*(1-t)*t 
-					+ 3.0 * p2 * (1 - t)* t * t 
+        var res =   p0*(1-t)*(1-t)*(1-t)
+					+ 3.0 * p1 * (1-t)*(1-t)*t
+					+ 3.0 * p2 * (1 - t)* t * t
 					+ p3*t*t*t;
         return res;
 
@@ -23,8 +23,8 @@ function CubicBezier(P0, P1, P2, P3)
 function QuadraticBezier(P0, P1, P2)
  {
     let scalar = function(t, p0, p1, p2, p3){
-        var res =   p0*(1-t)*(1-t) 
-					+ 2.0 * p1 * (1-t)*t 
+        var res =   p0*(1-t)*(1-t)
+					+ 2.0 * p1 * (1-t)*t
 					+ p2*t*t;
         return res;
 
@@ -50,7 +50,7 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
     if( v0 == 0 )
 	{
         throw new Error('no change in velocity not implemented');
-    } 
+    }
 	// Terminal velocity is zero - fit with quadratic
     else if( vF ==  0)
 	{
@@ -69,7 +69,7 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
         let p1_y = (v0*p1_x);
         func = QuadraticBezier(P0, [p1_x, p1_y], P2);
     }
-	// terminal velocity higher than D/T or only just a little bit less that D/T 
+	// terminal velocity higher than D/T or only just a little bit less that D/T
 	// and hence requires some speed up towards the end
 	// needs a cubic bezier to fit
     else if( (vF > 0) && ((D - vF*T) <=  (1.0 * threshold * D) ) )
@@ -77,8 +77,8 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
         let P0 = [0.0, 0.0];
         let P1 = [D/V, D];
         let P3 = [T,D];
-        let p2_x = T - D/vF; 
-        let p2_y = 0.0; 
+        let p2_x = T - D/vF;
+        let p2_y = 0.0;
         let P2 = [p2_x, p2_y];
         let alpha = .75;
 
@@ -89,7 +89,7 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
 
         func = CubicBezier(P0, P1_adj, P2_adj, P3);
     }
-	// terminal velocity is close to D/T and simply produces a straightline equal to D/T 
+	// terminal velocity is close to D/T and simply produces a straightline equal to D/T
 	// does not seem like a good answer
 	// THIS SHOULD BE OBSOLETE
     else if( (vF > 0) && ((D - vF*T) <= (threshold * D) ) && ((D - vF*T) >=  (-1.0 * threshold * D) ) )
@@ -98,8 +98,8 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
         let P0 = [0.0, 0.0];
         let P1 = [D/V, D];
         let P3 = [T,D];
-        let p2_x = T - D/vF; 
-        let p2_y = 0.0; 
+        let p2_x = T - D/vF;
+        let p2_y = 0.0;
         let P2 = [p2_x, p2_y];
         let alpha = .75;
 
@@ -108,14 +108,14 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
 		// attempts to add a stretch factor .. seems to work for alpha 0.0 .. 1.0
         let P2_adj = [T - D*alpha/vF, D*(1.0 - alpha)]; // alpha 0 .. 1
 
-        func = CubicBezier(P0, P1_adj, P2_adj, P3);	
+        func = CubicBezier(P0, P1_adj, P2_adj, P3);
     }
 	// should not be any more cases
     else
 	{
         throw new Error('dont know what to do -- not implemented');
     }
-	
+
 	// this function is the trajectory of the initial velocity
     this.tangent_initial = function(t)
 	{
@@ -130,7 +130,7 @@ BezDecelerator.class = function Decelerator(v0, vF, tF, dF)
     }.bind(this);
 
 	/*
-	* p is a parameter in the range 0..T - its the time point 
+	* p is a parameter in the range 0..T - its the time point
 	*/
     this.getDistance = function(p)
     {
